ad522d93897b0a541d2b2dcdd2ca58163f1473c1
Jesse <jesse_108@163.com>
2016-08-02 18:18:05 +0800

    update

--- c/01_base.c
+++ c/01_base.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
+#include <stdio.h> //stdio.h 标准输如输出库   #include 引入  
 
-int main(int argc, char** argv) { //程序执行主函数
+//now frist is Hello World
+int main(int argc, char** argv) { //程序执行主函数 argc 参数个数,argv 参数列表 
 	printf("Hello world!\n");
+	
+	
+	
 	return 0;
 }
 
