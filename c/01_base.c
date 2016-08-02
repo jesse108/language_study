0e275c02cf9f9fb15671cae4c3cbf4afabf0fbc3
zhaojian@mac_pro <jesse_108@163.com>
2016-08-02 22:38:27 +0800

    update

--- c/01_base.c
+++ c/01_base.c
@@ -2,10 +2,16 @@
 
 //now frist is Hello World
 int main(int argc, char** argv) { //程序执行主函数 argc 参数个数,argv 参数列表 
-	printf("Hello world!\n");
-	
-	
-	
-	return 0;
+    printf("Hello world!\n");// printf 是stdio 提供的输出方法
+    
+    //////下面是C的基础数据类型
+    int number = 123; //int 型
+    char isFat = 'y';
+    float a1 = 0.11;
+    double pi = 3.1415926;
+//    bool isBoy = true; //这会报错, 也就是说C没有布尔型
+     	
+
+    return 0;
 }
 
