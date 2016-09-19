a506506617aacb7003145895d6475d576b5a1edf
zhaojian@mac_pro <jesse_108@163.com>
2016-09-19 08:03:29 +0800

    update

--- c/01_base.c
+++ c/01_base.c
@@ -9,8 +9,17 @@ int main(int argc, char** argv) { //程序执行主函数 argc 参数个数,argv 参数列表
     char isFat = 'y';
     float a1 = 0.11;
     double pi = 3.1415926;
+
+    //标准化输出
+    printf("%d \n",12);// %d 整数
+    printf("%c \n",'a');// %c char 字符
+    printf("%s \n","abcdes");// %s 字符串
+    printf("%f %f\n",1.23,3.22);//%f 6位小数
+
+
 //    bool isBoy = true; //这会报错, 也就是说C没有布尔型
-     	
+    int isTrue = 1 > 2;
+    printf("isTrue:%d \n",isTrue);     	
 
     return 0;
 }
